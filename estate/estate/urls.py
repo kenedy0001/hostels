9079eadc0ebc31654a22e4fcd7396b1e6679fbb0
kenedy001 <kenedykombo20@gmail.com>
2020-09-24 20:56:10 +0300

    first commit

---
+++ estate/estate/urls.py
@@ -0,0 +1,12 @@
+from allauth.account.views import confirm_email
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('account/', include('allauth.urls')),
+    path('accounts-rest/registration/account-confirm-email/(?P<key>.+)/$', confirm_email, name='account_confirm_email'),
+]
